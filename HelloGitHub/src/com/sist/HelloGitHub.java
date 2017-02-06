a8afb06718446925e596cda0e971b0376f19f637
jungousam <jungousam@naver.com>
2017-02-06 14:26:08 +0900

    HelloGitHub.java modified ob Locale

--- HelloGitHub/src/com/sist/HelloGitHub.java
+++ HelloGitHub/src/com/sist/HelloGitHub.java
@@ -1,5 +1,12 @@
 package com.sist;
 
 public class HelloGitHub {
-
+	public static void main(String[] args){
+		System.out.println("Hello Git");
+		System.out.println("What`s Git");
+		System.out.println("It's DVCS");
+		System.out.println("It's collaboration");
+		System.out.println("Conflict");
+		
+	}
 }
